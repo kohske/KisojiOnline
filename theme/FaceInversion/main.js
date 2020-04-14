9f2c90e55077bbbd057c020c5bf45ee741595b85
kohske <takahashi.kohske@gmail.com>
2020-04-15 01:19:13 +0900

    顔認知修正（課題分割等）

--- theme/FaceInversion/main.js
+++ theme/FaceInversion/main.js
@@ -1,20 +1,38 @@
+/**
+ * キソジオンライン 顔認知実験
+ *
+ * by kohske takahashi
+ *
+ * MIT License | https://github.com/kohske/KisojiOnline/blob/master/LICENSE
+ */
+
 // 実験パラメータ
 var learn_dur = 3000; // 学習画像の呈示時間
 var learn_eval_dur = 3000; // 学習セッションの回答時間
 
-// 刺激は親HTMLの方で定義（練習用もできるように）
+//var learn_dur = 30; // 学習画像の呈示時間
+//var learn_eval_dur = 30; // 学習セッションの回答時間
 
+
+// 刺激は親HTMLの方で定義（練習用もできるように）
+// var n_learn = 24; // 学習セッションの試行数（刺激を全部使うとは限らない）
 // 刺激シャッフル
-upfaces = jsPsych.randomization.repeat(upfaces, 1); // 正立顔
-invfaces = jsPsych.randomization.repeat(invfaces, 1);// 倒立顔
+// var upface_images = [];
+// var invface_images = [];
+//
+// exp_type: b => 両方, s => 形状のみ, m => 意味のみ
+// var exp_type = ""; // both
+var upfaces = jsPsych.randomization.shuffle(upfaces_images); // 正立顔
+var invfaces = jsPsych.randomization.shuffle(invfaces_images); // 倒立顔
+
 // それぞれ呈示刺激と未呈示刺激のペア
 var upface_pairs = [];
 for (var i=0; i<upfaces.length/2; ++i) {
-  upface_pairs.push({f: upfaces[i], s: upfaces[i+upfaces.length/2]});
+  upface_pairs.push({updown: "正立", f: upfaces[i], s: upfaces[i+upfaces.length/2]});
 }
 var invface_pairs = [];
 for (var i=0; i<invfaces.length/2; ++i) {
-  invface_pairs.push({f: invfaces[i], s: invfaces[i+upfaces.length/2]});
+  invface_pairs.push({updown: "倒立", f: invfaces[i], s: invfaces[i+upfaces.length/2]});
 }
 
 var shape_stim = []; // 形状課題での刺激
@@ -26,10 +44,10 @@ for (var i=0; i<upface_pairs.length/2; ++i) {
   mean_stim.push(invface_pairs[i+invface_pairs.length/2]);  
 }
 
-shape_stim = jsPsych.randomization.repeat(shape_stim, 1); // 形状学習用
-shape_recall_stim = jsPsych.randomization.repeat(shape_stim, 1); // 形状再認用
-mean_stim = jsPsych.randomization.repeat(mean_stim, 1); // 意味学習用
-mean_recall_stim = jsPsych.randomization.repeat(mean_stim, 1); // 意味再認用
+shape_stim = jsPsych.randomization.shuffle(shape_stim); // 形状学習用
+shape_recall_stim = jsPsych.randomization.shuffle(shape_stim); // 形状再認用
+mean_stim = jsPsych.randomization.shuffle(mean_stim); // 意味学習用
+mean_recall_stim = jsPsych.randomization.shuffle(mean_stim); // 意味再認用
 
 
 // 手続き
@@ -44,6 +62,7 @@ var shape_learn_inst = {
     "顔写真が消えた後、マウスで数字のボタンをクリックしてください。<br/><br/>"+
     "スペースキーを押すと始まります。"+
     "</p>",
+  post_trial_gap: 500,      
 };
 
 // 意味学習セッションの教示
@@ -56,6 +75,7 @@ var mean_learn_inst = {
     "顔写真が消えた後、マウスで数字のボタンをクリックしてください。<br/><br/>"+
     "スペースキーを押すと始まります。"+
     "</p>",
+  post_trial_gap: 500,    
 };
 
 // 再認セッションの教示
@@ -68,6 +88,7 @@ var recall_inst = {
     "スペースキーを押すと始まります。"+
     "</p>",
   choices: ['space'],
+  post_trial_gap: 500,      
 };
 
 // 学習セッションの画像呈示
@@ -91,18 +112,18 @@ var learn_eval = {
   choices: ["1","2","3","4","5","6","7","8","9","10"],
   prompt: "",
   response_ends_trial: false,
-  trial_duration: learn_eval_dur,  
-  
+  trial_duration: learn_eval_dur,
+ 
   data: {
     record: 1,
     task: jsPsych.timelineVariable("task"),
     type: "eval",
-    img: jsPsych.timelineVariable('f'),    
+    img: jsPsych.timelineVariable('f'),
   },
   on_start: function(trial){
-    if (trial.data.task == "shape") {
+    if (trial.data.task == "形態") {
       trial.prompt = "<br/>鼻の高さは？(1: とても低い - 10: とても高い)";
-    } else if (trial.data.task == "mean") {
+    } else if (trial.data.task == "意味") {
       trial.prompt = "<br/>信頼性は？(1: とても低い - 10: とても高い)";
     }
     ct = trial;
@@ -117,10 +138,12 @@ var recall_show = {
   img1: jsPsych.timelineVariable('f'),
   img2: jsPsych.timelineVariable('s'),
   choices: ['leftarrow', 'rightarrow'],
+  post_trial_gap: 500,
   data: {
     record: 1,
     task: jsPsych.timelineVariable("task"),
     type: "recall",
+    updown: jsPsych.timelineVariable('updown'),    
   },
   on_start: function(trial){
     var cr = Math.random()>0.5 ? "L": "R";
@@ -142,14 +165,9 @@ var recall_show = {
     else if (data.key_press == 39) {
       data.resp = "R";      
     }
+    if (data.correct == data.resp) {data.answer = 1;}
+    else {data.answer = 0;}    
   },
-};
-
-var blank = {
-  type: 'html-keyboard-response',
-  stimulus: [""],
-  choices: jsPsych.NO_KEYS,
-  trial_duration: 1000,
 };
 
 // 形状学習セッション
@@ -166,38 +184,69 @@ var mean_learn_proc = {
 
 // 形状再認セッション
 var shape_recall_proc = {
-  timeline: [recall_show, blank],
+  timeline: [recall_show],
   timeline_variables: shape_recall_stim,
 }
 
 // 意味再認セッション
 var mean_recall_proc = {
-  timeline: [recall_show, blank],
+  timeline: [recall_show],
   timeline_variables: mean_recall_stim,  
 }
 
 // 形状セッション
 var shape_proc = {
-  timeline: [shape_learn_inst, blank, shape_learn_proc, recall_inst, blank, shape_recall_proc],
-  timeline_variables: [{task: "shape"}],
+  timeline: [shape_learn_inst, shape_learn_proc, recall_inst, shape_recall_proc],
+  timeline_variables: [{task: "形態"}],
 };
 
 // 意味セッション
 var mean_proc = {
-  timeline: [mean_learn_inst, blank, mean_learn_proc, recall_inst, blank, mean_recall_proc],
-  timeline_variables: [{task: "mean"}],  
+  timeline: [mean_learn_inst, mean_learn_proc, recall_inst, mean_recall_proc],
+  timeline_variables: [{task: "意味"}],  
 };
 
 
-jsPsych.init({
-  timeline: Math.random()>0.5 ? [shape_proc, mean_proc] : [mean_proc, shape_proc],
+var tl;
+if (exp_type == "b") {
+  tl = Math.random()>0.5 ? [shape_proc, mean_proc] : [mean_proc, shape_proc];
+} else if (exp_type == "s") {
+  tl = [shape_proc];
+} else if (exp_type == "m") {
+  tl = [mean_proc];
+}
 
-  on_finish: function(){
-    document.getElementById("result").style.display = "block";
-    
+// 実験終了時の画面
+var finish_experiment_procedure = {
+  type: 'html-keyboard-response',
+  stimulus: "",
+  choices: jsPsych.NO_KEYS,  
+  on_start: function(trial) {
+    var txt = "";
     var dt = jsPsych.data.get().filter([{record: 1}]);
-    dt = dt.ignore(["response_type", "key_press", "avg_frame_time", "trial_type", "trial_index", "time_elapsed", "internal_node_id", "record", "stimulus"]);
-    var txt = dt.csv().replace(/,/g, "\t").replace(/"/g,"")
-    document.getElementById("result").value=txt;
+    dt = dt.ignore(["rt", "stimulus", "response_type", "key_press", "avg_frame_time", "trial_type", "trial_index", "time_elapsed", "internal_node_id", "record"]);
+
+    txt += "判断課題\t顔の向き\t正答率\n";    
+    var tasks  = exp_type == "b" ? ["意味", "形態"] : (exp_type == "s" ? ["形態"] : ["意味"]);
+    var ud = ["正立", "倒立"];
+    for (t in tasks) {
+      for (v in ud) {
+	var ave = jsPsych.data.get().filter([{task: tasks[t], updown: ud[v]}]).select("answer").mean();
+	txt += tasks[t]+"\t"+ud[v]+"\t"+ave+"\n";
+      }
+    }
+    txt += "\n\n---------------------\n\n";
+    txt += dt.csv().replace(/,/g, "\t").replace(/"/g,"");
+    
+    trial.stimulus = '<p>実験終了です。</p><p>下の枠の中のデータをエクセルなどに貼り付けて保存しましょう。</p>'+
+      '<p>枠の中をクリックしてから、Ctrl+A (コントロールキーを押しながらAキーを押す)ですべて選択し、Ctrl+Cでクリップボードにコピーできます。</p>'+
+      '<p>コピーしたら、新しいエクセルファイルを開き、Ctrl+Vで貼付けましょう。</p>'+
+      '<textarea style="width:450px; height: 300px">'+txt+'</textarea>';
+
+    
   }
-});
+};
+
+tl.push(finish_experiment_procedure);
+
+jsPsych.init({ timeline: tl });
